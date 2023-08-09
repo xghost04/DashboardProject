6a53bf18a54a8f878338f91a039d0d2c3e1d6015
EL MEHDI <elmehdi.saa123@outlook.com>
2023-08-09 20:08:07 +0100

    index
    ...

---
+++ orders.js
@@ -0,0 +1,22 @@
+const Orders = [
+
+     {
+        productName: 'JavaScript Tutorial',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+     },
+     {
+        productName: 'CSS Full Copurse',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+     },
+     {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+     },
+
+]
